b39dbc9538a49eba0218e22314dc266f418c28be
Pawanrawat123 <121912771+Pawanrawat123@users.noreply.github.com>
2026-04-01 11:37:00 -0400

    Add script.js for Yoga Website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Navbar toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
